2f1da1e785ae9127623f6b510594eff917a224a5
Ravi kumar <raviroy44405@gmail.com>
2025-04-16 08:52:19 +0530

    Add files via upload

---
+++ string/stringcompare.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<string.h>
+int main ()
+{
+    char a[]="hello";
+    char b[]="world";
+    int cmp =strcmp(a,b);
+    printf("comparison result =%d",cmp);
+    return 0;
+    
+}
